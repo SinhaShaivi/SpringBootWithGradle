4414121a60b8295340fc9045b9d0748be70abcd8
shaivi <shaivisinha09@gmail.com>
2020-12-17 15:32:59 +0530

    Used Gradle

---
+++ DemoGradle/src/main/java/com/cg/bms/hello/HelloSpringBoot.java
@@ -0,0 +1,14 @@
+package com.cg.bms.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBoot {
+    @RequestMapping("/hello")
+	public String helloSpring()
+	{
+		System.out.println("Hello World");
+	    return "Welcome!!";
+	}
+}
