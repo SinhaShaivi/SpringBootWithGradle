559ec6cac6d320917c0d2207aaff28942160d048
SinhaShaivi <76103528+SinhaShaivi@users.noreply.github.com>
2021-05-09 22:53:51 +0530

    Update HelloSpringBoot.java

--- DemoGradle/src/main/java/com/cg/bms/hello/HelloSpringBoot.java
+++ DemoGradle/src/main/java/com/cg/bms/hello/HelloSpringBoot.java
@@ -3,6 +3,7 @@ package com.cg.bms.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //System.out.println("hi");
+//testing this
 @RestController
 public class HelloSpringBoot {
     @RequestMapping("/hello")
